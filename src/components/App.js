fc0bd2a2788b2f741448df99c5c1665697e3329b
William Caballero <wcaballero4@gmail.com>
2017-08-03 00:53:57 -0500

    final V1

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import marked from 'marked';
+import {gfmInitText} from './gfmInitText';
 
-console.log(marked("__markdwon__"));
+// setting up markdown to work with github flavor markdown
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+});
+
 
 export default class App extends Component {
   // can run methods but not vanilla js
@@ -11,20 +23,23 @@ export default class App extends Component {
 
     // init state to manage data flow
     this.state = {
-      markdown: ''
+      markdown: gfmInitText
     };
   }
+
   render() {
+
     return (
       <div className="App container">
         <div className="row">
           <textarea
             type="text"
-            className="col-xs-6"
+            className="col-xs-12 col-lg-6"
             value= {this.state.markdown}
             onChange = {(event) => {this.setState({markdown: event.target.value});}}
           />
-          <div className="col-xs-6">{this.state.markdown}</div>
+          <div className="col-xs-12 col-lg-offset-1 col-lg-5"
+            dangerouslySetInnerHTML= {{__html: marked(this.state.markdown)}} />
         </div>
       </div>
 
